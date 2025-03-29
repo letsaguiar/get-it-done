ad8c7707b74c50100634b5b0919d0cb3c9df527c
Letícia Aguiar <letsaguiar@yandex.com>
2025-03-28 22:59:24 -0300

    if applied, implement a commit strategy at UserPreferenceStore

--- src/stores/user-preferences.store.ts
+++ src/stores/user-preferences.store.ts
@@ -1,14 +1,28 @@
 import { create } from "zustand";
 
+const Store = "UserPreference"
+
 type State = {
 	darkMode: boolean;
 }
 
 type Actions = {
 	toggleDarkMode: () => void;
+	commit: () => void;
 }
 
-export const useUserPreferencesStore = create<State & Actions>((set) => ({
+export const useUserPreferencesStore = create<State & Actions>((set, get) => ({
 	darkMode: false,
-	toggleDarkMode: () => set((state) => ({ darkMode: !state.darkMode }))
+	commit: () => {
+		const data: State = {
+			darkMode: get().darkMode
+		};
+
+		localStorage.setItem(Store, JSON.stringify(data));
+	},
+	toggleDarkMode: () => {
+		set(state => ({ darkMode: !state.darkMode }));
+		get().commit();
+	},
+	...JSON.parse(localStorage.getItem(Store) || '{}')
 }))

693d025274de20be308c6962b2fe99c3f4e6100b
Letícia Aguiar <letsaguiar@yandex.com>
2025-03-30 00:59:36 -0300

    if applied, create prioritizing-view and add it to router.tsx

---
+++ src/app/prioritizing/index.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function PrioritizingView() {
+	return <>
+		<h1>Hello World!</h1>
+	</>
+}
--- src/router.tsx
+++ src/router.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import App from "./app/layout";
 import ListingView from "./app/listing";
+import PrioritizingView from "./app/prioritizing";
 
 const router = createBrowserRouter([
 	{
 		path: '/',
 		Component: App,
 		children: [
-			{ path: '/listing', Component: ListingView }
+			{ path: '/listing', Component: ListingView },
+			{ path: 'prioritizing', Component: PrioritizingView }
 		]
 	}
 ])

305576609f38062a9da57d024e202155ef34482b
Letícia Aguiar <letsaguiar@yandex.com>
2025-03-30 10:52:05 -0300

    if applied, setup sortable mechanism

--- src/app/prioritizing/index.tsx
+++ src/app/prioritizing/index.tsx
@@ -1,7 +1,56 @@
-import React from "react"
+import React, {useState} from 'react';
+import {
+  DndContext, 
+  closestCenter,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+  DragEndEvent,
+} from '@dnd-kit/core';
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+} from '@dnd-kit/sortable';
+import { SortableItem } from '@/components/task-input/SortableTaskInput';
 
 export default function PrioritizingView() {
-	return <>
-		<h1>Hello World!</h1>
-	</>
+	const [items, setItems] = useState([1, 2, 3]);
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+
+  return (
+    <DndContext 
+      sensors={sensors}
+      collisionDetection={closestCenter}
+      onDragEnd={handleDragEnd}
+    >
+      <SortableContext 
+        items={items}
+        strategy={verticalListSortingStrategy}
+      >
+        {items.map(id => <SortableItem key={id} id={id} />)}
+      </SortableContext>
+    </DndContext>
+  );
+  
+  function handleDragEnd(event: DragEndEvent) {
+    const {active, over} = event;
+    
+    if (over && active.id !== over.id) {
+      setItems((items) => {
+        const oldIndex = items.indexOf(active.id as number);
+        const newIndex = items.indexOf(over.id as number);
+        
+        return arrayMove(items, oldIndex, newIndex);
+      });
+    }
+  }
+
 }
